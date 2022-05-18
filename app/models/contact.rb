fab7eae0521f77bbe905f9f85d90668ba8469be1
rodsaal <rodrigo.rsaal@gmail.com>
2022-05-18 09:13:50 -0300

    feat(phone): Add associations between phone and contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,11 +1,37 @@
 class Contact < ApplicationRecord
 
-  belongs_to :kind
-  def author
-    "Rodrigo Almeida"
-  end
+  #Associations
+  belongs_to :kind, optional: true
+  has_many :phones
 
   def as_json(options={})
-    super(methods: :author, root: true)
+    hash = super(options)
+    hash[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
+    hash
   end
+
+  # def to_br
+  #   {
+  #     name: self.name,
+  #     email: self.email,
+  #     birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?),
+  #     phone: self.phones
+  #   }
+  # end
+
+  # def author
+  #   "Rodrigo Almeida"
+  # end
+  #
+  # def kind_description
+  #   self.kind.description
+  # end
+  #
+  # def as_json(options={})
+  #   super(
+  #     root: true,
+  #     methods: [:kind_description, :author],
+  #     include: {kind: {only: :description}}
+  #     )
+  # end
 end
