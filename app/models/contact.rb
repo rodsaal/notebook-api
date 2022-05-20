3b791ee39340a8b868c0f3888784f8846f08cd72
rodsaal <rodrigo.rsaal@gmail.com>
2022-05-20 17:28:43 -0300

    feat(phones): Crud of phones

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,10 @@
 class Contact < ApplicationRecord
 
   #Associations
-  belongs_to :kind, optional: true
+  belongs_to :kind
   has_many :phones
+
+  accepts_nested_attributes_for :phones, allow_destroy: true
 
   def as_json(options={})
     hash = super(options)
