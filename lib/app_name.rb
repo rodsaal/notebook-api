1e8f28eb7afccbe9b28dd372a28477ec0ed1a9ea
rodsaal <rodrigo.rsaal@gmail.com>
2024-06-17 22:19:50 -0300

    feat[rack]: adding rack middleware

---
+++ lib/app_name.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class AppName
+  def initialize(app, app_name)
+    @app = app
+    @app_name = app_name
+  end
+
+  def call(env)
+    status, headers, response = @app.call(env)
+    headers.merge!('X-App-Name' => "#{@app_name}")
+    [status, headers, [response.body]]
+  end
+end
